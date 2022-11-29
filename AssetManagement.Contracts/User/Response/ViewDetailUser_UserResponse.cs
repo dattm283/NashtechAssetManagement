8a2d5268483358a40b59e0ee5fb775da771c2fe0
Sơn Trần <phamsn2001@gmail.com>
2022-11-29 13:16:58 +0000

    Merged PR 43: Fix dto us 22

--- AssetManagement.Contracts/User/Response/ViewDetailUser_UserResponse.cs
+++ AssetManagement.Contracts/User/Response/ViewDetailUser_UserResponse.cs
@@ -11,8 +11,6 @@ namespace AssetManagement.Contracts.User.Response
         public DateTime DateOfBirth { get; set; }
         public DateTime JoinedDate { get; set; }
         public string Gender { get; set; }
-        public DateTime JoinedDate { get; set; }
-        public string Type { get; set; }
         public string Location { get; set; }
     }
 }
