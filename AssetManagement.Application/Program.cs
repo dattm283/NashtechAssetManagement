c1cbd2631cd5a629531653b70c1e40bf4e9ebdca
Minh Đạt Trần <dattm283@gmail.com>
2022-12-01 10:53:35 +0000

    Merged PR 57: Implement swagger on deloyed azure web
    implement swagger on deloyed azure web

--- AssetManagement.Application/Program.cs
+++ AssetManagement.Application/Program.cs
@@ -118,11 +118,11 @@ var app = builder.Build();
 
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+// if (app.Environment.IsDevelopment())
+// {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+// }
 
 app.UseCors("corsapp");
 
