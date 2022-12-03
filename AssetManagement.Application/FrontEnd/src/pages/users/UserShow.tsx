188c174b09e9d0c4247ea5833565185e8b80aba7
Quang-Dobe <tdquangcr@gmail.com>
2022-12-03 10:44:41 +0700

    Update List user view

--- AssetManagement.Application/FrontEnd/src/pages/users/UserShow.tsx
+++ AssetManagement.Application/FrontEnd/src/pages/users/UserShow.tsx
@@ -1,26 +1,38 @@
-import React from "react";
-import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
-import { Box, Typography, Modal, IconButton } from '@mui/material';
-import { theme } from '../../theme'
+import {
+Box,
+Modal,
+Typography,
+IconButton,
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { theme } from "../../theme";
+import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
+import { getAssignementByAssetCodeId } from "../../services/assignment";
 
 // Style for Modal
 const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
+    position: "absolute",
+    top: "50%",
+    left: "50%",
     m: "0px",
     package: "0px",
-    transform: 'translate(-50%, -50%)',
-    display:'flex', flexDirection:'column',
-    width: 600,
-    bgcolor: 'background.paper',
-    border: '0px solid white',
-    borderRadius: '10px',
+    transform: "translate(-50%, -50%)",
+    display: "flex",
+    flexDirection: "column",
+    width: 650,
+    bgcolor: "background.paper",
+    border: "0px solid white",
+    borderRadius: "10px",
     boxShadow: 24,
 };
 
+const StyledModal = styled(Modal)`
+    .MuiBackdrop-root {
+        background-color: transparent;
+}
+`;
 
-// Capitalize string
 function isDateAndRestyle(sentence) {
     if (sentence === null){
         return "Unknown"
@@ -35,95 +47,125 @@ function isDateAndRestyle(sentence) {
     return sentence
 }
 
-// Params: openDetail:object, setOpenDetail:function, label:string, data:object
-function UserShow({ openDetail, setOpenDetail, label }) {
+const UserShow = ({ openDetail, setOpenDetail, label }) => {
     return (
-        <Modal
-            open={openDetail.status}
-            onClose={() => setOpenDetail({ status:false, data:{} })}
-            aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description"
-        >
-            <Box sx={style} border='none'>
-                <Box 
-                    sx={{ 
-                        m:"0px", 
-                        padding:"15px 0px", 
-                        display:"flex", 
-                        bgcolor:"lightgray", 
-                        borderBottom:"1px solid black",
-                        borderTopLeftRadius:"10px",
-                        borderTopRightRadius:"10px"
-                    }}
-                >
-                    <Box 
+        <>
+            <StyledModal
+                open={openDetail.status}
+                onClose={() => setOpenDetail({ status:false, data:{} })}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style} border="none">
+                    <Box
                         sx={{
-                            m:"auto",
+                        m: "0px",
+                        padding: "15px 0px",
+                        display: "flex",
+                        bgcolor: "#EFF1F5",
+                        borderTopLeftRadius: "10px",
+                        borderTopRightRadius: "10px",
+                        }}
+                    >
+                        <Box
+                        sx={{
+                            m: "auto 80px",
                             display: "flex",
                             flexDirection: "row",
                             justifyContent: "space-between",
-                            width: "440px",
+                            width: "540px",
                         }}
-                    >
-                        <Typography 
-                            id="modal-modal-title" 
-                            variant="h5" 
-                            component="h5" 
+                        >
+                        <Typography
+                            id="modal-modal-title"
+                            variant="h5"
+                            component="h5"
                             color={theme.palette.secondary.main}
                             width="280"
                             fontWeight="bold"
                         >
-                            {label}
+                            Detailed User Information
                         </Typography>
 
                         <IconButton
                             onClick={(e) => setOpenDetail({ status:false, data:{} })}
                             sx={{
-                                p:'0px',
-                                color:theme.palette.secondary.main,
-                                border:`2px solid ${theme.palette.secondary.main}`,
-                                borderRadius:"3px",
+                            p: "0px",
+                            color: theme.palette.secondary.main
                             }}
                         >
-                            <CloseRoundedIcon sx={{ fontWeight:"bold" }}  />
+                            <CloseRoundedIcon sx={{ fontWeight: "bold" }} />
                         </IconButton>
+                        </Box>
+                    </Box>
+
+                    <Box
+                        sx={{
+                        display: "flex",
+                        flexDirection: "row",
+                        m: "10px 80px",
+                        borderBottomLeftRadius: "10px",
+                        borderBottomRightRadius: "10px",
+                        }}
+                    >
+                        <Box flex="true" flexDirection="column">
+                            <Typography variant="subtitle1" sx={{ m: "20px 0px" }}>
+                                Staff Code
+                            </Typography>
+                            <Typography variant="subtitle1" sx={{ m: "20px 0px" }}>
+                                Full Name
+                            </Typography>
+                            <Typography variant="subtitle1" sx={{ m: "20px 0px" }}>
+                                User Name
+                            </Typography>
+                            <Typography variant="subtitle1" sx={{ m: "20px 0px" }} noWrap={true}>
+                                Date of Birth
+                            </Typography>
+                            <Typography variant="subtitle1" sx={{ m: "20px 0px" }}>
+                                Gender
+                            </Typography>
+                            <Typography variant="subtitle1" sx={{ m: "20px 0px" }} noWrap={true}>
+                                Joined Date
+                            </Typography>
+                            <Typography variant="subtitle1" sx={{ m: "20px 0px" }}>
+                                Type
+                            </Typography>
+                            <Typography variant="subtitle1" sx={{ m: "20px 0px" }}>
+                                Location
+                            </Typography>
+                        </Box>
+
+                        <Box flex="true" flexDirection="column">
+                            <Typography variant="subtitle1" sx={{ m: "20px 60px" }}>
+                                {openDetail.data["staffCode"]}
+                            </Typography>
+                            <Typography variant="subtitle1" sx={{ m: "20px 60px" }}>
+                                {openDetail.data["fullName"]}
+                            </Typography>
+                            <Typography variant="subtitle1" sx={{ m: "20px 60px" }}>
+                                {openDetail.data["userName"]}
+                            </Typography>
+                            <Typography variant="subtitle1" sx={{ m: "20px 60px" }}>
+                                {isDateAndRestyle(openDetail.data["dateOfBirth"])}
+                            </Typography>
+                            <Typography variant="subtitle1" sx={{ m: "20px 60px" }}>
+                                {openDetail.data["gender"]}
+                            </Typography>
+                            <Typography variant="subtitle1" sx={{ m: "20px 60px" }}>
+                                {isDateAndRestyle(openDetail.data["joinedDate"])}
+                            </Typography>
+                            <Typography variant="subtitle1" sx={{ m: "20px 60px" }}>
+                                {openDetail.data["type"]}
+                            </Typography>
+                            <Typography variant="subtitle1" sx={{ m: "20px 60px" }}>
+                                {openDetail.data["location"] == "HoChiMinh" ? "HCM" : openDetail.data["location"] == "HaNoi" ? "HN" : openDetail.data["location"]}
+                            </Typography>
+                        </Box>
                     </Box>
                 </Box>
+            </StyledModal>
+        </>
+    );
+};
 
-                <Box 
-                    sx={{ 
-                        display:"flex", 
-                        flexDirection:"row", 
-                        m:"10px 80px",
-                        borderBottomLeftRadius:"10px",
-                        borderBottomRightRadius:"10px"
-                    }}
-                >
-                    <Box flex="true" flexDirection="column" >
-                        <Typography variant="h6" sx={{ m:"10px 0px"}}>Staff Code</Typography>
-                        <Typography variant="h6" sx={{ m:"10px 0px"}}>Full Name</Typography>
-                        <Typography variant="h6" sx={{ m:"10px 0px"}}>User Name</Typography>
-                        <Typography variant="h6" sx={{ m:"10px 0px"}}>Date of Birth</Typography>
-                        <Typography variant="h6" sx={{ m:"10px 0px"}}>Gender</Typography>
-                        <Typography variant="h6" sx={{ m:"10px 0px"}}>Joined Date</Typography>
-                        <Typography variant="h6" sx={{ m:"10px 0px"}}>Type</Typography>
-                        <Typography variant="h6" sx={{ m:"10px 0px"}}>Location</Typography>
-                    </Box>
-                    
-                    <Box flex="true" flexDirection="column" >
-                        <Typography variant="h6" sx={{ m:"10px 50px"}}>{openDetail.data["staffCode"]}</Typography>
-                        <Typography variant="h6" sx={{ m:"10px 50px"}}>{openDetail.data["fullName"]}</Typography>
-                        <Typography variant="h6" sx={{ m:"10px 50px"}}>{openDetail.data["userName"]}</Typography>
-                        <Typography variant="h6" sx={{ m:"10px 50px"}}>{isDateAndRestyle(openDetail.data["dateOfBirth"])}</Typography>
-                        <Typography variant="h6" sx={{ m:"10px 50px"}}>{openDetail.data["gender"]}</Typography>
-                        <Typography variant="h6" sx={{ m:"10px 50px"}}>{isDateAndRestyle(openDetail.data["joinedDate"])}</Typography>
-                        <Typography variant="h6" sx={{ m:"10px 50px"}}>{openDetail.data["type"]}</Typography>
-                        <Typography variant="h6" sx={{ m:"10px 50px"}}>{openDetail.data["location"] == "HoChiMinh" ? "HCM" : openDetail.data["location"] == "HaNoi" ? "HN" : openDetail.data["location"]}</Typography>
-                    </Box>
-                </Box>
-            </Box>
-        </Modal>
-    )
-}
-
-export default UserShow
+export default UserShow;
