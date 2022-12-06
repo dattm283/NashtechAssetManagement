11c0ca8035c259e27be5b9201aca074cabded2e3
Sơn Trần <phamsn2001@gmail.com>
2022-12-06 06:28:51 +0000

    Merged PR 105: Admin can only view asset with same location
    Admin can only view asset with same location

--- AssetManagement.Application/Controllers/AssetsController.cs
+++ AssetManagement.Application/Controllers/AssetsController.cs
@@ -141,9 +141,11 @@ namespace AssetManagement.Application.Controllers
             [FromQuery] string? order = "ASC",
             [FromQuery] string? createdId = "")
         {
+            var username = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Name).Value;
+            var user = _dbContext.Users.FirstOrDefault(x => x.UserName == username);
             var list = _dbContext.Assets
                 .Include(x => x.Category)
-                .Where(x => !x.IsDeleted);
+                .Where(x => !x.IsDeleted && x.Location == user.Location);
             if (!string.IsNullOrEmpty(searchString))
             {
                 list = list.Where(x => x.Name.ToUpper().Contains(searchString.ToUpper()) || x.AssetCode.ToUpper().Contains(searchString.ToUpper()));
