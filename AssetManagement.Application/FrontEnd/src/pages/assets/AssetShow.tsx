4abcc9e406b95f52e3bd5061c5d85022a0cc1d7c
Nghia Trong <nghia.t6363@gmail.com>
2022-12-07 03:53:26 +0000

    Merged PR 113: fix bug detail asset ui
    fix bug detail asset ui

--- AssetManagement.Application/FrontEnd/src/pages/assets/AssetShow.tsx
+++ AssetManagement.Application/FrontEnd/src/pages/assets/AssetShow.tsx
@@ -1,8 +1,9 @@
 import {
-    Box,
-    Modal,
-    Typography,
-    IconButton,
+  IconButton,
+  Grid,
+  Dialog,
+  DialogContent,
+  DialogTitle,
 } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import Table from "@mui/material/Table";
@@ -14,222 +15,192 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import styled from "styled-components";
 import { theme } from "../../theme";
-import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
 import { getAssignementByAssetCodeId } from "../../services/assignment";
+import {
+  SimpleShowLayout,
+  TextField,
+  FunctionField,
+  DateField,
+} from "react-admin";
+import DataLabel from "../../components/grid/DataLabel";
+import DataText from "../../components/grid/DataText";
+import CloseIcon from "@mui/icons-material/Close";
 
-// Style for Modal
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  m: "0px",
-  package: "0px",
-  transform: "translate(-50%, -50%)",
-  display: "flex",
-  flexDirection: "column",
-  width: 750,
-  bgcolor: "background.paper",
-  border: "0px solid white",
-  borderRadius: "10px",
-  boxShadow: 24,
-};
-
-const StyledModal = styled(Modal)`
+const StyledDialog = styled(Dialog)`
   .MuiBackdrop-root {
     background-color: transparent;
   }
 `;
 
+const StyledDialogTitle = styled(DialogTitle)`
+  &.MuiDialogTitle-root {
+    padding: 16px 38px;
+    background-color: #eff1f5;
+    color: #cf2338;
+  }
+`;
+
+const StyledDialogContent = styled(DialogContent)`
+  &.MuiDialogContent-root {
+    background-color: #fafcfc;
+  }
+`;
+
 const AssetShow = (props) => {
-    const { isOpened, toggle, record = {} } = props;
-    const [assignements, setAssignments] = useState([]);
+  const { isOpened, toggle, record = {} } = props;
+  const [assignements, setAssignments] = useState([]);
 
-    useEffect(() => {
-        if (record) {
-            getAssignementByAssetCodeId(record.id)
-                .then((response) => {
-                    setAssignments(response);
-                })
-                .catch(() => { });
-        }
-    }, [record]);
+  const style = {
+    bgcolor: "#cf2338",
+    fontWeight: "bold",
+    color: "#fff",
+  };
 
-    function isDateAndRestyle(sentence) {
-        if (sentence === null) {
-            return "Unknown"
-        }
-        if (typeof sentence === 'string') {
-            var parsedDate = Date.parse(sentence);
-            if (!isNaN(parsedDate)) {
-                var [mm, dd, yyyy] = sentence.split("T")[0].split("/")
-                console.log(`${dd}/${mm}/${yyyy}`)
-                return `${dd}/${mm}/${yyyy}`;
-            }
-        }
-        return sentence
+  useEffect(() => {
+    if (record) {
+      getAssignementByAssetCodeId(record.id)
+        .then((response) => {
+          setAssignments(response);
+        })
+        .catch(() => {});
     }
+  }, [record]);
 
-    return (
-        <>
-            <StyledModal
-                open={isOpened}
-                onClose={toggle}
-                sx={{}}
-                aria-labelledby="modal-modal-title"
-                aria-describedby="modal-modal-description"
-            >
-                <Box sx={style} border="none">
-                    <Box
-                        sx={{
-                            m: "0px",
-                            padding: "15px 0px",
-                            display: "flex",
-                            bgcolor: "#EFF1F5",
-                            borderTopLeftRadius: "10px",
-                            borderTopRightRadius: "10px",
-                        }}
-                    >
-                        <Box
+  function isDateAndRestyle(sentence) {
+    if (sentence === null) {
+      return "Unknown";
+    }
+    if (typeof sentence === "string") {
+      var parsedDate = Date.parse(sentence);
+      if (!isNaN(parsedDate)) {
+        var [mm, dd, yyyy] = sentence.split("T")[0].split("/");
+        console.log(`${dd}/${mm}/${yyyy}`);
+        return `${dd}/${mm}/${yyyy}`;
+      }
+    }
+    return sentence;
+  }
+
+  return (
+    <>
+      <StyledDialog
+        maxWidth="md"
+        open={isOpened}
+        onClose={toggle}
+        scroll="body"
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <StyledDialogTitle id="alert-dialog-title" sx={style}>
+          {"Detail Asset Information"}
+          <IconButton
+            aria-label="close"
+            onClick={toggle}
+            sx={{
+              position: "absolute",
+              right: 8,
+              top: 8,
+              color: "#CF2338",
+            }}
+          >
+            <CloseIcon />
+          </IconButton>
+        </StyledDialogTitle>
+        <StyledDialogContent>
+          <SimpleShowLayout record={record}>
+            <Grid container spacing={2}>
+              <DataLabel label="Asset Code" />
+              <DataText>
+                <TextField source="assetCode" />
+              </DataText>
+              <DataLabel label="Asset Name" />
+              <DataText>
+                <TextField source="name" />
+              </DataText>
+              <DataLabel label="Category" />
+              <DataText>
+                <TextField source="categoryName" />
+              </DataText>
+              <DataLabel label="Installed Date" />
+              <DataText>
+                <DateField source="installedDate" locales="en-GB" />
+              </DataText>
+              <DataLabel label="State" />
+              <DataText>
+                <FunctionField
+                  render={(record) =>
+                    record.state == "NotAvailable"
+                      ? "Not available"
+                      : record.state == "WaitingForRecycling"
+                      ? "Waiting for recycling"
+                      : record.state
+                  }
+                />
+              </DataText>
+              <DataLabel label="Location" />
+              <DataText>
+                <DataText>
+                  <FunctionField
+                    render={(record) =>
+                      record.location == "HoChiMinh"
+                        ? "HCM"
+                        : record.location == "HaNoi"
+                        ? "HN"
+                        : record.location
+                    }
+                  />
+                </DataText>
+              </DataText>
+              <DataLabel label="Specification" />
+              <DataText>
+                <TextField source="specification" />
+              </DataText>
+              <DataLabel label="History" />
+              <DataText>
+                <TableContainer component={Paper}>
+                  <Table aria-label="simple table">
+                    <TableHead>
+                      <TableRow>
+                        <TableCell>Date</TableCell>
+                        <TableCell align="right">Assigned to</TableCell>
+                        <TableCell align="right">Assigned by</TableCell>
+                        <TableCell align="right">Returned Date</TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {Array.prototype.map.bind(assignements)(
+                        (row: any, index) => (
+                          <TableRow
+                            key={index}
                             sx={{
-                                m: "auto",
-                                display: "flex",
-                                flexDirection: "row",
-                                justifyContent: "space-between",
-                                width: "540px",
+                              "&:last-child td, &:last-child th": { border: 0 },
                             }}
-                        >
-                            <Typography
-                                id="modal-modal-title"
-                                variant="h5"
-                                component="h5"
-                                color={theme.palette.secondary.main}
-                                width="280"
-                                fontWeight="bold"
-                            >
-                                Detailed Asset Information
-                            </Typography>
-
-                            <IconButton
-                                onClick={toggle}
-                                sx={{
-                                    p: "0px",
-                                    color: theme.palette.secondary.main
-                                }}
-                            >
-                                <CloseRoundedIcon sx={{ fontWeight: "bold" }} />
-                            </IconButton>
-                        </Box>
-                    </Box>
-
-                    <Box
-                        sx={{
-                            display: "flex",
-                            flexDirection: "row",
-                            m: "10px 80px",
-                            borderBottomLeftRadius: "10px",
-                            borderBottomRightRadius: "10px",
-                        }}
-                    >
-                        <Box flex="true" flexDirection="column">
-                            <Typography variant="subtitle1" sx={{ m: "20px 0px" }}>
-                                Asset Code
-                            </Typography>
-                            <Typography variant="subtitle1" sx={{ m: "20px 0px" }}>
-                                Asset Name
-                            </Typography>
-                            <Typography variant="subtitle1" sx={{ m: "20px 0px" }}>
-                                Category
-                            </Typography>
-                            <Typography
-                                variant="subtitle1"
-                                sx={{ m: "20px 0px" }}
-                                noWrap={true}
-                            >
-                                Installed Date
-                            </Typography>
-                            <Typography variant="subtitle1" sx={{ m: "20px 0px" }}>
-                                State
-                            </Typography>
-                            <Typography variant="subtitle1" sx={{ m: "20px 0px" }}>
-                                Location
-                            </Typography>
-                            <Typography variant="subtitle1" sx={{ m: "20px 0px" }}>
-                                Specification
-                            </Typography>
-                            <Typography variant="subtitle1" sx={{ m: "20px 0px" }}>
-                                History
-                            </Typography>
-                        </Box>
-
-                        <Box flex="true" flexDirection="column">
-                            <Typography variant="subtitle1" sx={{ m: "20px 50px" }}>
-                                {record.assetCode}
-                            </Typography>
-                            <Typography variant="subtitle1" sx={{ m: "20px 50px" }}>
-                                {record.name}
-                            </Typography>
-                            <Typography variant="subtitle1" sx={{ m: "20px 50px" }}>
-                                {record.categoryName}
-                            </Typography>
-                            <Typography variant="subtitle1" sx={{ m: "20px 50px" }}>
-                                {isDateAndRestyle(new Date(record.installedDate).toLocaleDateString())}
-                            </Typography>
-                            <Typography variant="subtitle1" sx={{ m: "20px 50px" }}>
-                                {record.state == "NotAvailable"
-                                    ? "Not available"
-                                    : record.state == "WaitingForRecycling"
-                                        ? "Waiting for recycling"
-                                        : record.state}
-                            </Typography>
-                            <Typography variant="subtitle1" sx={{ m: "20px 50px" }}>
-                                {record.location == "HoChiMinh"
-                                    ? "HCM"
-                                    : record.location == "HaNoi"
-                                        ? "HN"
-                                        : record.location}
-                            </Typography>
-                            <Typography
-                                variant="subtitle1"
-                                sx={{ m: "20px 50px" }}
-                                noWrap={true}
-                            >
-                                {record.specification}
-                            </Typography>
-                            <TableContainer sx={{ mt: "20px", mb: "20px" }} component={Paper}>
-                                <Table aria-label="simple table">
-                                    <TableHead>
-                                        <TableRow>
-                                            <TableCell>Date</TableCell>
-                                            <TableCell align="right">Assigned to</TableCell>
-                                            <TableCell align="right">Assigned by</TableCell>
-                                            <TableCell align="right">Returned Date</TableCell>
-                                        </TableRow>
-                                    </TableHead>
-                                    <TableBody>
-                                        {Array.prototype.map.bind(assignements)((row: any, index) => (
-                                            <TableRow
-                                                key={index}
-                                                sx={{
-                                                    "&:last-child td, &:last-child th": { border: 0 },
-                                                }}
-                                            >
-                                                <TableCell component="th" scope="row">
-                                                    {isDateAndRestyle(row.assignedDate)}
-                                                </TableCell>
-                                                <TableCell align="right">{row.assignedTo}</TableCell>
-                                                <TableCell align="right">{row.assignedBy}</TableCell>
-                                                <TableCell align="right">{isDateAndRestyle(row.returnedDate)}</TableCell>
-                                            </TableRow>
-                                        ))}
-                                    </TableBody>
-                                </Table>
-                            </TableContainer>
-                        </Box>
-                    </Box>
-                </Box>
-            </StyledModal>
-        </>
-    );
+                          >
+                            <TableCell component="th" scope="row">
+                              {isDateAndRestyle(row.assignedDate)}
+                            </TableCell>
+                            <TableCell align="right">
+                              {row.assignedTo}
+                            </TableCell>
+                            <TableCell align="right">
+                              {row.assignedBy}
+                            </TableCell>
+                            <TableCell align="right">
+                              {isDateAndRestyle(row.returnedDate)}
+                            </TableCell>
+                          </TableRow>
+                        )
+                      )}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+              </DataText>
+            </Grid>
+          </SimpleShowLayout>
+        </StyledDialogContent>
+      </StyledDialog>
+    </>
+  );
 };
 
 export default AssetShow;
